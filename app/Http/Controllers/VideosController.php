1dc522428da47462013316547e004a8f67bc60a0
berg1618 <berg@blickdev.com>
2023-09-15 16:53:25 -0300

    feat: criar models e VideoController

---
+++ app/Http/Controllers/VideosController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Video;
+use Illuminate\Http\Request;
+
+class VideosController extends Controller
+{
+    public function index()
+    {
+        Video::find(1);
+    }
+
+    public function store(Request $request)
+    {
+        $video = new Video();
+        $video= $request;
+
+        $video->save();
+    }
+
+    public function show(Video $video)
+    {
+        //
+    }
+
+ 
+    public function update(Request $request, Video $video)
+    {
+        //
+    }
+
+    public function destroy(Video $video)
+    {
+        //
+    }
+}
---
+++ app/Models/Video.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Video extends Model
+{
+    protected $table = 'videos';
+
+    protected $fillable = [
+        'id',
+        'name',
+        'user_id',
+        'description',
+        "created_at",
+        "updated_at",
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
