6c0f0dd6a8869d25b96945814fa7e73c5da88a0a
berg1618 <berg@blickdev.com>
2023-09-20 17:57:44 -0300

    adicionar estilos a página inicial e imagens

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,11 +10,37 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="{{asset('resources/css/style.css')}}">
-
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
+    <header class="flex items-center p-6 justify-between">
+        <a href="#">
+            <img src="logo.png" alt="YouTube Logo" class="h-8" />
+        </a>
+        <form class="flex mt-0 mr-8 mb-0 ml-16">
+            <input class="p-[.4em] border-solid border-gray-[777] border-2 rounded-tl-3xl rounded-bl-3xl w-full"
+                type="search" placeholder="Search" aria-label="Search" />
+            <button type="submit"
+                class="flex p-0 pl-5 items-center border-solid border-gray-[777] border-2 bg-[f8f8f8] rounded-tr-3xl rounded-br-3xl bl-0 cursor-pointer">
+                <img src="magnify.svg"/>
+            </button>
+        </form>
+        <div class="grid items-center gap-6 grid-cols-4">
+            <a href="#">
+                <img src="video-plus.svg" alt="Upload Video" />
+            </a>
+            <a href="#">
+                <img src="apps.svg" alt="Apps" />
+            </a>
+            <a href="#">
+                <img src="bell.svg" alt="Notifications" />
+            </a>
+            <a href="#">
+                <img class="w-9 h-9 rounded-3xl" src="bangela.jpeg" alt="Your Channel" />
+            </a>
+        </div>
+    </header>
     @yield('content')
 </body>
 
--- resources/views/videos/index.blade.php
+++ resources/views/videos/index.blade.php
@@ -1,6 +1,26 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1>list of all videos on youtube ever</h1>
-    {{ $videos }}
+    <div> {{ $videos }} </div>
+    <section class="grid grid-cols-4 gap-y-6 gap-x-80	">
+        <article class="flex flex-col">
+            <a href="#" class="flex relative" data-duration="12:24">
+                <img class="w-100% h-100% bg-[aaa] min-w-250 min-h-150" src="http://unsplash.it/250/150?gravity=center" />
+            </a>
+            <div class="flex items-start mt-4">
+                <a href="#">
+                    <img class="mr-3 rounded-[50%] w-9 h-9 bg-[aaa]" src="http://unsplash.it/36/36?gravity=center" />
+                </a>
+                <div class="flex flex-col">
+                    <a href="#" class="font-bold black text-[1.1rem]">Video Title</a>
+                    <a href="#" class="mb-0.5 transition-colors">Channel Name</a>
+                    <div class="text-[555]">
+                        <span>12K views</span>
+                        •
+                        <span>1 week ago</span>
+                    </div>
+                </div>
+            </div>
+        </article>
+    </section>
 @endsection
