9df6548f4e06fa7d3caa6570f1c0d11f2d1d5f65
berg1618 <berg@blickdev.com>
2023-09-22 09:29:51 -0300

    adicionar foreach na listagem

--- resources/views/videos/index.blade.php
+++ resources/views/videos/index.blade.php
@@ -1,26 +1,29 @@
 @extends('layouts.main')
 
 @section('content')
-    <div> {{ $videos }} </div>
-    <section class="grid grid-cols-4 gap-y-6 gap-x-80	">
-        <article class="flex flex-col">
-            <a href="#" class="flex relative" data-duration="12:24">
-                <img class="w-100% h-100% bg-[aaa] min-w-250 min-h-150" src="http://unsplash.it/250/150?gravity=center" />
-            </a>
-            <div class="flex items-start mt-4">
-                <a href="#">
-                    <img class="mr-3 rounded-[50%] w-9 h-9 bg-[aaa]" src="http://unsplash.it/36/36?gravity=center" />
+    <section class="grid grid-cols-4 gap-y-6 gap-x-12 px-12 py-0 mx-0 my-6 border-t-4 border-solid border-[ccc]">
+
+        @foreach ($videos as $video)
+            <article class="flex flex-col">
+                <a href="#" class="flex relative" data-duration="00:00">
+                    <img class="w-100% h-100% bg-[aaa] min-w-250 min-h-150" src="http://unsplash.it/250/150?gravity=center" />
                 </a>
-                <div class="flex flex-col">
-                    <a href="#" class="font-bold black text-[1.1rem]">Video Title</a>
-                    <a href="#" class="mb-0.5 transition-colors">Channel Name</a>
-                    <div class="text-[555]">
-                        <span>12K views</span>
-                        •
-                        <span>1 week ago</span>
+                <div class="flex items-start mt-4">
+                    <a href="#">
+                        <img class="mr-3 rounded-[50%] w-9 h-9 bg-[aaa]" src="http://unsplash.it/36/36?gravity=center" />
+                    </a>
+                    <div class="flex flex-col">
+                        <a href="#" class="font-bold black text-[1.1rem]">{{$video->name}}</a>
+                        <a href="#" class="mb-0.5 transition-colors">{{$video->user->name}}</a>
+                        <div class="text-[555]">
+                            <span>12K views</span>
+                            •
+                            <span>{{$video->created_at}}</span>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </article>
+            </article>
+        @endforeach
+
     </section>
 @endsection
